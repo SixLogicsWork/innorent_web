a84a56f159a1f47c7ffd4331f0fe657a0d194467
Muhammad Zain Tariq <zain.tariq@sixlogics.com>
2023-10-25 14:53:59 +0500

    third commit

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "b1eb940b6dc6e46af6793cce2576eb41",
-"index.html": "b577e52ee7790ce55639dcce22217f95",
-"/": "b577e52ee7790ce55639dcce22217f95",
+"index.html": "5e9cedc98a37d35b8b42196687a2e8ea",
+"/": "5e9cedc98a37d35b8b42196687a2e8ea",
 "main.dart.js": "5061a6b0bc375b49be1f49cfca4ff27f",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",

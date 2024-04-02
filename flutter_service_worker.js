a505fc7eb957d9976fafac1a8c494839bd073d2b
Muhammad Zain Tariq <zain.tariq@sixlogics.com>
2024-04-02 11:31:35 +0500

    qa in progress

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,10 +4,10 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "b1eb940b6dc6e46af6793cce2576eb41",
-"index.html": "228e09d4545d764d24640632b6f6d309",
-"/": "228e09d4545d764d24640632b6f6d309",
+"index.html": "18badb15ea5f4bdf951bd65a77530ed4",
+"/": "18badb15ea5f4bdf951bd65a77530ed4",
 "firebase-messaging-sw.js": "4dc9cc14dbeea446bb4953b62981244d",
-"main.dart.js": "8c98343b3b107104459a45797485ecdf",
+"main.dart.js": "27f857f33ba499585c1af3fba28713f9",
 "flutter.js": "c71a09214cb6f5f8996a531350400a9a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
